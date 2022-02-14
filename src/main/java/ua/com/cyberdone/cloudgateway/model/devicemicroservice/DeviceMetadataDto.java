f16fe9b8b2df48ad7c9eb74a6cbb6715d43a29d6
Ostap Komplikevych <ostap.ja@gmail.com>
2022-02-14 02:01:12 +0200

    [cdgs-03] add hydroponic Setting template endpoints

--- src/main/java/ua/com/cyberdone/cloudgateway/model/devicemicroservice/DeviceMetadataDto.java
+++ src/main/java/ua/com/cyberdone/cloudgateway/model/devicemicroservice/DeviceMetadataDto.java
@@ -4,32 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
-import static ua.com.cyberdone.devicemicroservice.validation.ValidationConstants.VALUE_IS_BLANK_MSG;
-import static ua.com.cyberdone.devicemicroservice.validation.ValidationConstants.VALUE_IS_NULL_MSG;
-import static ua.com.cyberdone.devicemicroservice.validation.ValidationConstants.VALUE_NOT_NUMBER_MSG;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class DeviceMetadataDto {
-    @NotNull(message = VALUE_IS_NULL_MSG)
-    @Digits(message = VALUE_NOT_NUMBER_MSG, integer = Integer.MAX_VALUE, fraction = 10)
     private Long id;
-    @NotBlank(message = VALUE_IS_BLANK_MSG)
     private String uuid;
-    @NotBlank(message = VALUE_IS_BLANK_MSG)
     private String name;
-    @NotBlank(message = VALUE_IS_BLANK_MSG)
     private String description;
-    @NotNull(message = VALUE_IS_NULL_MSG)
     private DeviceType deviceType;
-    @NotNull(message = VALUE_IS_NULL_MSG)
     private Boolean accessEnabled;
-    @Digits(message = VALUE_NOT_NUMBER_MSG, integer = Integer.MAX_VALUE, fraction = 10)
-    @NotNull(message = VALUE_IS_NULL_MSG)
     private Long userId;
 }
